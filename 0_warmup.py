1fa46e0963243a1a927d964decb6fea97b9c4da0
Jeff Cohen <cohen.jeff@gmail.com>
2016-02-11 16:23:35 -0600

    Add a hint

--- 0_warmup.py
+++ 0_warmup.py
@@ -4,6 +4,7 @@
 
 # 1. Can you display a list of all of the book titles?
 
+# HINT: See https://github.com/mpcs50101/week3/blob/master/3_abstraction.py
 
 
 # 2. Can you display the list in alphabetical order?
